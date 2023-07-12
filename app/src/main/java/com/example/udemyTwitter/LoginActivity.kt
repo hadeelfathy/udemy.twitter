68b7c97976902f3f00750818de5048c205917c56
Ghazouly <maguedfathey@gmail.com>
2023-07-13 00:02:06 +0300

    second commit

--- app/src/main/java/com/example/udemyTwitter/LoginActivity.kt
+++ app/src/main/java/com/example/udemyTwitter/LoginActivity.kt
@@ -2,11 +2,10 @@ package com.example.udemyTwitter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.udemyTwitter.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
