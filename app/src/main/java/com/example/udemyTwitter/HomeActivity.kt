da3bd704dd5380c55b1df718c672917a77eee77f
Ghazouly <maguedfathey@gmail.com>
2023-07-13 22:56:17 +0300

    fourth commit

---
+++ app/src/main/java/com/example/udemyTwitter/HomeActivity.kt
@@ -0,0 +1,22 @@
+package com.example.udemyTwitter
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+  companion object{
+      fun newIntent(context: Context)=Intent(context,HomeActivity::class.java)
+  }
+
+
+
+
+
+}
--- app/src/main/java/com/example/udemyTwitter/LoginActivity.kt
+++ app/src/main/java/com/example/udemyTwitter/LoginActivity.kt
@@ -1,11 +1,85 @@
 package com.example.udemyTwitter
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.widget.Toast
+import com.google.android.play.integrity.internal.e
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : AppCompatActivity() {
+
+    private val firebaseAuth=FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+
+    fun onLogin(v:View){
+
+        var proceed = true
+
+        if (emailLET.text.isNullOrEmpty()){
+            emailTEL.error="Email required"
+            emailTEL.isErrorEnabled= true
+           proceed= false
+        }
+
+        if (passwordLET.text.isNullOrEmpty()){
+            passwordTEL.error="password required"
+            passwordTEL.isErrorEnabled= true
+            proceed= false
+        }
+
+        if (proceed){
+            loginProgressLayout.visibility=View.VISIBLE
+            firebaseAuth.signInWithEmailAndPassword(emailLET.text.toString(),passwordLET.text.toString())
+
+                .addOnCompleteListener{ task->
+                    if(!task.isSuccessful) {
+                        loginProgressLayout.visibility= View.GONE
+                        Toast.makeText(this@LoginActivity,"Login error:${task.exception?.localizedMessage}",Toast.LENGTH_SHORT)
+                    }
+
+
+
+
+
+                }
+
+                .addOnFailureListener{e  ->
+                     e.printStackTrace()
+                    loginProgressLayout.visibility= View.GONE
+
+                }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+   fun goToSignUp(v: View){
+
+   }
+
+
+
+
+
+
 }
