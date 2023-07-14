f9ad4bd7ebbbc4076bb5372f9ce2b398e1ea692c
Ghazouly <maguedfathey@gmail.com>
2023-07-14 03:59:43 +0300

    fifth commit

--- app/src/main/java/com/example/udemyTwitter/LoginActivity.kt
+++ app/src/main/java/com/example/udemyTwitter/LoginActivity.kt
@@ -1,10 +1,16 @@
 package com.example.udemyTwitter
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputLayout
 import com.google.android.play.integrity.internal.e
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
@@ -12,10 +18,47 @@ class LoginActivity : AppCompatActivity() {
 
     private val firebaseAuth=FirebaseAuth.getInstance()
 
+    private val firebaseAuthListener= FirebaseAuth.AuthStateListener {
+        val user= firebaseAuth.currentUser?.uid
+            user?.let { startActivity(HomeActivity.newIntent(this)) }
+            finish()
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+       setTextChangedListener(emailLET,emailTEL)
+       setTextChangedListener(passwordLET,passwordTEL)
+
+       loginProgressLayout.setOnTouchListener { v, event -> true  }
+
     }
+
+    fun setTextChangedListener(et:EditText,til:TextInputLayout){
+        et.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                  til.isErrorEnabled= false
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        )
+
+    }
+
+
 
 
     fun onLogin(v:View){
@@ -77,9 +120,20 @@ class LoginActivity : AppCompatActivity() {
 
    }
 
+    override fun onStart() {
+        super.onStart()
+      firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
+
+    }
+   companion object{
+       fun newIntent(context: Context)= Intent(context,LoginActivity::class.java)
 
 
-
-
+   }
 
 }
