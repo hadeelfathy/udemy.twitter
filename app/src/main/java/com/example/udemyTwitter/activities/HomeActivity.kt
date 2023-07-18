beed41aba9d24b37e08a5fbfa6e2522108893952
Ghazouly <maguedfathey@gmail.com>
2023-07-18 20:14:11 +0300

    eighth commit

--- app/src/main/java/com/example/udemyTwitter/activities/HomeActivity.kt
+++ app/src/main/java/com/example/udemyTwitter/activities/HomeActivity.kt
@@ -5,16 +5,53 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import com.example.udemyTwitter.R
+import com.example.udemyTwitter.fragments.HomeFragment
+import com.example.udemyTwitter.fragments.MyActivityFragment
+import com.example.udemyTwitter.fragments.SearchFragment
+import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     private val firebaseAuth= FirebaseAuth.getInstance()
 
+
+    private var sectionPagerAdapter:SectionPagerAdapter?=null
+   private val homeFragment= HomeFragment()
+   private val searchFragment= SearchFragment()
+   private val myActivityFragment= MyActivityFragment()
+
+    private val userId= FirebaseAuth.getInstance().currentUser?.uid
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    sectionPagerAdapter=SectionPagerAdapter(supportFragmentManager)
+     container.adapter= sectionPagerAdapter
+     container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+      tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
+      tabs.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
+          override fun onTabSelected(tab: TabLayout.Tab?) {
+              TODO("Not yet implemented")
+          }
+
+          override fun onTabUnselected(tab: TabLayout.Tab?) {
+              TODO("Not yet implemented")
+          }
+
+          override fun onTabReselected(tab: TabLayout.Tab?) {
+              TODO("Not yet implemented")
+          }
+      })
+
+
+
     }
 
     fun onLogout(v: View){
@@ -23,7 +60,34 @@ class HomeActivity : AppCompatActivity() {
         finish()
     }
 
+    override fun onResume() {
+        super.onResume()
+      if (userId==null){
+          startActivity(LoginActivity.newIntent(this))
+         finish()
+      }
 
+
+    }
+
+
+
+
+
+    inner class SectionPagerAdapter(fm:FragmentManager):FragmentPagerAdapter(fm){
+        override fun getCount() = 3
+
+        override fun getItem(position: Int): Fragment {
+            return when(position){
+                0-> homeFragment
+                1-> searchFragment
+                else-> myActivityFragment
+            }
+
+
+                    }
+
+    }
 
 
 
---
+++ app/src/main/java/com/example/udemyTwitter/fragments/TwitterFragment.kt
@@ -0,0 +1,6 @@
+package com.example.udemyTwitter.fragments
+
+import androidx.fragment.app.Fragment
+
+abstract class TwitterFragment :Fragment(){
+}
