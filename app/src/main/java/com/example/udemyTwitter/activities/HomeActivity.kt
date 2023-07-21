b501f2d259f1714f232586d399d75165f8409b4d
Ghazouly <maguedfathey@gmail.com>
2023-07-21 08:55:24 +0300

    tenth commit

--- app/src/main/java/com/example/udemyTwitter/activities/HomeActivity.kt
+++ app/src/main/java/com/example/udemyTwitter/activities/HomeActivity.kt
@@ -26,7 +26,7 @@ class HomeActivity : AppCompatActivity() {
    private val searchFragment= SearchFragment()
    private val myActivityFragment= MyActivityFragment()
 
-    private val userId= FirebaseAuth.getInstance().currentUser?.uid
+    private var userId= FirebaseAuth.getInstance().currentUser?.uid
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,15 +56,12 @@ class HomeActivity : AppCompatActivity() {
 
     }
 
-    fun onLogout(v: View){
-        firebaseAuth.signOut()
-        startActivity(LoginActivity.newIntent(this))
-        finish()
-    }
+
 
     override fun onResume() {
         super.onResume()
-      if (userId==null){
+        userId= FirebaseAuth.getInstance().currentUser?.uid
+        if (userId==null){
           startActivity(LoginActivity.newIntent(this))
          finish()
       }
--- app/src/main/java/com/example/udemyTwitter/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/udemyTwitter/activities/ProfileActivity.kt
@@ -2,30 +2,98 @@ package com.example.udemyTwitter.activities
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.udemyTwitter.R
+import com.example.udemyTwitter.util.DATA_USERS
+import com.example.udemyTwitter.util.DATA_USER_EMAIL
+import com.example.udemyTwitter.util.DATA_USER_USERNAME
+import com.example.udemyTwitter.util.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseDB = FirebaseFirestore.getInstance()
+    private val userId = FirebaseAuth.getInstance().currentUser?.uid
+    private val firebaseStorage= FirebaseStorage.getInstance()
+    private var imageUrl:String?= null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        if (userId == null) {
+            finish()
+
+        }
+        profileProgressLayout.setOnTouchListener { v, event -> true }
+        populateInfo()
     }
 
-  fun onApply(v:View){}
+    fun populateInfo(){
 
-    fun onSignOut(v:View){}
+      profileProgressLayout.visibility= View.VISIBLE
+      firebaseDB.collection(DATA_USERS).document(userId!!).get()
+          .addOnSuccessListener {  documentSnapshot->
+              val user= documentSnapshot.toObject(User::class.java)
+              usernameET.setText(user?.username,TextView.BufferType.EDITABLE)
+              emailET.setText(user?.email,TextView.BufferType.EDITABLE)
+              imageUrl?.let {  }
+              profileProgressLayout.visibility= View.GONE
 
+          }
 
+          .addOnFailureListener { e->
+              e.printStackTrace()
+              finish()
+          }
 
-
-
-    companion object{
-        fun newIntent(context: Context)= Intent(context,ProfileActivity::class.java)
     }
 
 
+
+
+    fun onApply(v: View) {
+        profileProgressLayout.visibility= View.VISIBLE
+        val username = usernameET.text.toString()
+        val email= emailET.text.toString()
+        val map= HashMap<String,Any>()
+        map[DATA_USER_USERNAME]= username
+        map[DATA_USER_EMAIL]= email
+
+        firebaseDB.collection(DATA_USERS).document(userId!!).update(map)
+            .addOnSuccessListener {
+                Toast.makeText(this,"Update successful",Toast.LENGTH_SHORT).show()
+                 finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this,"Update failed.Please try again",Toast.LENGTH_SHORT).show()
+                profileProgressLayout.visibility= View.GONE
+
+            }
+
+
+
+    }
+
+    fun onSignOut(v: View) {
+        firebaseAuth.signOut()
+        finish()
+    }
+
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, ProfileActivity::class.java)
+    }
 
 
 }
